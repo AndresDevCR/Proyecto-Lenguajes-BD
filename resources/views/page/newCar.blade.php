a0313dfe35f1de1bf2cc97a3b3bc610d431b1048
Andrés Vargas <85952546+Skippy696@users.noreply.github.com>
2022-11-14 18:16:57 -0600

    avance base de datos

---
+++ resources/views/page/newCar.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+
+@section('title', 'Find By ID')
+
+@section('content')
+    <div class="text-center display 1 color primary">
+
+        <h1>
+            Autos Nuevos
+        </h1>
+    </div>
+{{-- 
+    <div class="text-center">
+        <p>{{$car-> name}}</p>
+        <p>{{$car-> model}}</p>
+        <p>{{$car-> year}}</p>
+        <p>{{$car-> price}}</p>
+        <p>{{$car-> description}}</p>
+        <p>{{$car-> image}}</p>
+    </div>
+
+    <div class="text-center">
+        <a href="{{route('newCar')}}" class="btn btn-primary">Volver</a>
+    </div>
+ --}}
+
+
+@endsection
